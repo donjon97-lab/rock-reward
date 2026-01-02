61c1900f420eea62c7e41c1f5153ed0021c5f75c
donjon97-lab <jonathan.97cadangan@gmail.com>
2026-01-03 02:22:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const params = new URLSearchParams(window.location.search);
 const user = params.get("user");
-const rockId = params.get("rockId");
+
+// rockId dari URL = "126"
+const rockNumber = parseInt(params.get("rockId"), 10);
+
+// ubah jadi "rock126"
+const rockId = `rock${String(rockNumber).padStart(3, "0")}`;
 
 fetch("rocks.json")
   .then(res => res.json())
   .then(rocks => {
     const rock = rocks.find(r => r.id === rockId);
-    if (!rock) return;
+    if (!rock) {
+      console.error("Rock not found:", rockId);
+      return;
+    }
 
     const img = document.getElementById("rockImage");
     const text = document.getElementById("resultText");
